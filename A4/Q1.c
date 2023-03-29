cb8cb1022163947a90cf86d206a90906ec42de50
Jeena01 <jeenajavahar@gmail.com>
2023-03-28 21:59:37 -0700

    m

--- A4/Q1.c
+++ A4/Q1.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     // Implementation goes here.
     
     char* filename=argv[1];
+    printf("Reading file: %s\n",filename);
     FILE* f=fopen(filename,"r");
     if(f==NULL){
         printf("Invalid file.");
@@ -18,11 +19,15 @@ int main(int argc, char *argv[]) {
     unsigned int x;
     fscanf(f,"%d",&n);
     fscanf(f,"%d",&m);
+    unsigned int mask= (1 << m )-1;
+    int i=1;
     while(fscanf(f,"%d",&x)==1){
 
-        d= x & (pow(2,m)-1);
+        d= x & mask;
         p=x >> m;
-        printf("Virtual address %d is in page number %d and offset %d",x,p,d);
+        printf("Virtual address v%d %d is in page number %d and offset %d\n",i++,x,p,d);
 
     }
+    fclose(f);
+    return (0);
 }
